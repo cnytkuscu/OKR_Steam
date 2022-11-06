575d8dfce323b47f6733e73947afe56d866c28ad
cnytkuscu <kuscu.cnyt@gmail.com>
2022-11-06 16:30:33 +0300

    Added multiple Controllers, Database Tables and migrations and methods.

---
+++ OKR_Steam/Validators/UserValidators/UserValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using OKR_Steam.Models.RequestModels;
+
+namespace OKR_Steam.Validators.UserValidators
+{
+    public class UserValidator :AbstractValidator<SteamProfileRequestModel>
+    {
+        public UserValidator()
+        {
+            RuleFor(x => x.Username)
+                .NotEmpty()
+                .WithMessage("Username Parametresi boş olamaz.");
+               
+
+        }
+    }
+}
